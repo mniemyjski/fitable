27ffddbaffc6088df391d1f69dfd014424a4c970
Marcel Niemyjski <niemyjski.marcel@gmail.com>
2022-04-10 12:26:30 +0200

    changed easy-localizations to intl and arb

---
+++ lib/app/settings/cubit/locale_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'locale_state.dart';
+
+class LocaleCubit extends Cubit<LocaleState> {
+  LocaleCubit() : super(LocaleInitial());
+}
