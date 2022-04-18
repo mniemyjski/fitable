ff3603ce9e581fb8eec474d0292c2db4b0a529ea
Marcel Niemyjski <niemyjski.marcel@gmail.com>
2022-04-18 18:58:42 +0200

    created product_repository.dart

---
+++ lib/app/home/repositories/product_repository.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+
+import 'package:appwrite/appwrite.dart' as aw;
+import 'package:injectable/injectable.dart';
+import 'package:universal_io/io.dart';
+
+import '../../../constants/constants.dart';
+import '../../../utilities/services/app_write.dart';
+import '../../auth/models/auth_model.dart';
+import '../../failure/models/failure_model.dart';
+import '../../issues/models/issue_model.dart';
+import '../models/product/product_model.dart';
+
+abstract class BaseProductRepository {
+  Future<void> create({required Auth auth, required Product product});
+  Future<void> update({required Auth auth, required Issue issue});
+  Future<Product> get(String id);
+  Stream<List<Product>> stream(String id);
+}
+
+@Singleton()
+class ProductRepository extends BaseProductRepository {
+  final AppWrite _appWrite;
+  ProductRepository(this._appWrite) : super();
+
+  @override
+  Future<void> create({required Auth auth, required Product product}) async {
+    Future result = _appWrite.database.createDocument(
+      collectionId: Collections.products(),
+      documentId: 'unique()',
+      data: product.toJson(),
+      read: ['role:member'],
+      write: ['user:${auth.userId}'],
+    );
+
+    try {
+      await result;
+    } on SocketException catch (_) {
+      throw Failure('No Internet connection 😑');
+    } on HttpException catch (e) {
+      throw Failure(e.message);
+    } on aw.AppwriteException catch (e) {
+      throw Failure(e.message);
+    }
+  }
+
+  @override
+  Future<void> update({required Auth auth, required Issue issue}) async {
+    Future result = _appWrite.database.createDocument(
+      collectionId: Collections.issues(),
+      documentId: 'unique()',
+      data: issue.toJson(),
+      read: ['role:member'],
+      write: ['user:${auth.userId}'],
+    );
+
+    try {
+      await result;
+    } on SocketException catch (e) {
+      throw Failure('No Internet connection 😑');
+    } on HttpException catch (e) {
+      throw Failure(e.message);
+    } on aw.AppwriteException catch (e) {
+      throw Failure(e.message);
+    }
+  }
+
+  @override
+  Future<Product> get(String id) {
+    try {
+      // await result;
+    } on SocketException catch (_) {
+      throw Failure('No Internet connection 😑');
+    } on HttpException catch (e) {
+      throw Failure(e.message);
+    } on aw.AppwriteException catch (e) {
+      throw Failure(e.message);
+    }
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<List<Product>> stream(String id) {
+    try {
+      // await result;
+    } on SocketException catch (_) {
+      throw Failure('No Internet connection 😑');
+    } on HttpException catch (e) {
+      throw Failure(e.message);
+    } on aw.AppwriteException catch (e) {
+      throw Failure(e.message);
+    }
+    throw UnimplementedError();
+  }
+}
--- lib/config/injectable/injection.config.dart
+++ lib/config/injectable/injection.config.dart
@@ -7,9 +7,10 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../../app/account/repositories/account_repository.dart' as _i7;
+import '../../app/account/repositories/account_repository.dart' as _i8;
 import '../../app/account/repositories/avatar_repository.dart' as _i6;
 import '../../app/auth/repositories/auth_repository.dart' as _i5;
+import '../../app/home/repositories/product_repository.dart' as _i7;
 import '../../utilities/services/api.dart' as _i3;
 import '../../utilities/services/app_write.dart' as _i4;
 
@@ -26,7 +27,9 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   gh.singleton<_i4.AppWrite>(_i4.AppWriteProduction(), registerFor: {_prod});
   gh.singleton<_i5.AuthRepository>(_i5.AuthRepository(get<_i4.AppWrite>()));
   gh.singleton<_i6.AvatarRepository>(_i6.AvatarRepository(get<_i4.AppWrite>()));
-  gh.singleton<_i7.AccountRepository>(
-      _i7.AccountRepository(get<_i4.AppWrite>()));
+  gh.singleton<_i7.ProductRepository>(
+      _i7.ProductRepository(get<_i4.AppWrite>()));
+  gh.singleton<_i8.AccountRepository>(
+      _i8.AccountRepository(get<_i4.AppWrite>()));
   return get;
 }
