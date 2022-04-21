e0761a7fa101550fd151f335375fdac5d8bbb935
Marcel Niemyjski <niemyjski.marcel@gmail.com>
2022-04-21 21:44:40 +0200

    updated chooser date

--- lib/app/home/screens/home_screen.dart
+++ lib/app/home/screens/home_screen.dart
@@ -4,11 +4,13 @@ import 'package:fitable/app/home/widgets/head_activities.dart';
 import 'package:fitable/app/home/widgets/head_meals.dart';
 import 'package:fitable/app/home/widgets/head_measurement.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../config/routes/routes.gr.dart';
 import '../../../utilities/utilities.dart';
 import '../../../widgets/tile_expansion.dart';
 import '../../../widgets/widgets.dart';
+import '../cubit/change_date/change_date_cubit.dart';
 import '../widgets/date_chooser.dart';
 import '../widgets/element_meal.dart';
 import '../widgets/nutritional_basic_value.dart';
@@ -22,92 +24,101 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         appBar: customAppBar(title: AppLocalizations.of(context)!.home),
         drawer: CustomDrawer(),
-        body: Column(
-          children: [
-            DateChooser(),
-            Expanded(
-              child: ListView(
-                children: [
-                  TileExpansion(
+        body: BlocProvider(
+          create: (context) => ChangeDateCubit(),
+          child: _buildBody(),
+        ),
+        bottomSheet: NutritionalBasicValue(),
+        bottomNavigationBar: BottomAppBar(),
+      ),
+    );
+  }
+
+  Builder _buildBody() {
+    return Builder(builder: (context) {
+      return Column(
+        children: [
+          DateChooser(),
+          Expanded(
+            child: ListView(
+              children: [
+                TileExpansion(
+                  head: HeadMeals(
+                      name: AppLocalizations.of(context)!.breakfast,
+                      kcal: 0,
+                      proteins: 0,
+                      carbs: 0,
+                      fats: 0),
+                  list: [
+                    ElementMeal(style: Theme.of(context).textTheme.bodyText2),
+                    ElementMeal(style: Theme.of(context).textTheme.bodyText2),
+                    ElementMeal(style: Theme.of(context).textTheme.bodyText2),
+                    ElementMeal(style: Theme.of(context).textTheme.bodyText2),
+                  ],
+                  onPressed: () => _onPushToFavoriteProduct(context),
+                ),
+                TileExpansion(
                     head: HeadMeals(
-                        name: AppLocalizations.of(context)!.breakfast,
+                        name: AppLocalizations.of(context)!.lunch,
                         kcal: 0,
                         proteins: 0,
                         carbs: 0,
                         fats: 0),
                     list: [
                       ElementMeal(style: Theme.of(context).textTheme.bodyText2),
-                      ElementMeal(style: Theme.of(context).textTheme.bodyText2),
-                      ElementMeal(style: Theme.of(context).textTheme.bodyText2),
+                    ],
+                    onPressed: () => _onPushToFavoriteProduct(context)),
+                TileExpansion(
+                    head: HeadMeals(
+                        name: AppLocalizations.of(context)!.dinner,
+                        kcal: 0,
+                        proteins: 0,
+                        carbs: 0,
+                        fats: 0),
+                    list: [
                       ElementMeal(style: Theme.of(context).textTheme.bodyText2),
                     ],
-                    onPressed: () => _onPushToFavoriteProduct(context),
-                  ),
-                  TileExpansion(
-                      head: HeadMeals(
-                          name: AppLocalizations.of(context)!.lunch,
-                          kcal: 0,
-                          proteins: 0,
-                          carbs: 0,
-                          fats: 0),
-                      list: [
-                        ElementMeal(style: Theme.of(context).textTheme.bodyText2),
-                      ],
-                      onPressed: () => _onPushToFavoriteProduct(context)),
-                  TileExpansion(
-                      head: HeadMeals(
-                          name: AppLocalizations.of(context)!.dinner,
-                          kcal: 0,
-                          proteins: 0,
-                          carbs: 0,
-                          fats: 0),
-                      list: [
-                        ElementMeal(style: Theme.of(context).textTheme.bodyText2),
-                      ],
-                      onPressed: () => _onPushToFavoriteProduct(context)),
-                  TileExpansion(
-                      head: HeadMeals(
-                          name: AppLocalizations.of(context)!.supper,
-                          kcal: 0,
-                          proteins: 0,
-                          carbs: 0,
-                          fats: 0),
-                      list: [
-                        ElementMeal(style: Theme.of(context).textTheme.bodyText2),
-                      ],
-                      onPressed: () => _onPushToFavoriteProduct(context)),
-                  TileExpansion(
-                      head: HeadMeals(
-                          name: AppLocalizations.of(context)!.snack,
-                          kcal: 0,
-                          proteins: 0,
-                          carbs: 0,
-                          fats: 0),
-                      list: [
-                        ElementMeal(style: Theme.of(context).textTheme.bodyText2),
-                      ],
-                      onPressed: () => _onPushToFavoriteProduct(context)),
-                  TileExpansion(
-                      head: HeadActivities(),
-                      list: [],
-                      onPressed: () => context.router.push(
-                            FavoritesRoute(
-                              eTypeTabBar: ETypeTabBar.workout,
-                            ),
-                          )),
-                  TileExpansion(head: HeadMeasurement(), list: [], onPressed: () => null),
-                  SizedBox(
-                    height: 100,
-                  )
-                ],
-              ),
+                    onPressed: () => _onPushToFavoriteProduct(context)),
+                TileExpansion(
+                    head: HeadMeals(
+                        name: AppLocalizations.of(context)!.supper,
+                        kcal: 0,
+                        proteins: 0,
+                        carbs: 0,
+                        fats: 0),
+                    list: [
+                      ElementMeal(style: Theme.of(context).textTheme.bodyText2),
+                    ],
+                    onPressed: () => _onPushToFavoriteProduct(context)),
+                TileExpansion(
+                    head: HeadMeals(
+                        name: AppLocalizations.of(context)!.snack,
+                        kcal: 0,
+                        proteins: 0,
+                        carbs: 0,
+                        fats: 0),
+                    list: [
+                      ElementMeal(style: Theme.of(context).textTheme.bodyText2),
+                    ],
+                    onPressed: () => _onPushToFavoriteProduct(context)),
+                TileExpansion(
+                    head: HeadActivities(),
+                    list: [],
+                    onPressed: () => context.router.push(
+                          FavoritesRoute(
+                            eTypeTabBar: ETypeTabBar.workout,
+                          ),
+                        )),
+                TileExpansion(head: HeadMeasurement(), list: [], onPressed: () => null),
+                SizedBox(
+                  height: 100,
+                )
+              ],
             ),
-          ],
-        ),
-        bottomSheet: NutritionalBasicValue(),
-        bottomNavigationBar: BottomAppBar(),
-      ),
-    );
+          ),
+        ],
+      );
+    });
   }
 
   void _onPushToFavoriteProduct(BuildContext context) {
