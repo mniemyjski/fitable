3d179b6ad475e3cac5145b98c9c7b3a8eecfdd40
Marcel Niemyjski <niemyjski.marcel@gmail.com>
2022-04-21 21:42:03 +0200

    updated ip

--- lib/constants/paths.dart
+++ lib/constants/paths.dart
@@ -1,6 +1,6 @@
 class Paths {
   static String url_prod() => 'http://localhost:5001/v1';
-  static String url_dev() => 'http://192.168.135.157:5001/v1';
+  static String url_dev() => 'http://192.168.77.157:5001/v1';
 
   static String project_id_prod() => '6240d6724a75df195a79';
   static String project_id_dev() => '6240d6724a75df195a79';
