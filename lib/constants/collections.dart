a7238e9127cce0515164bde4cd0c64909e97ff6c
Marcel Niemyjski <niemyjski.marcel@gmail.com>
2022-05-14 15:42:44 +0200

    updated collections.dart

--- lib/constants/collections.dart
+++ lib/constants/collections.dart
@@ -1,6 +1,6 @@
 class Collections {
   static String accounts() => '6240db71daaa7f1ba453';
-  static String products() => '';
+  static String products() => '625d8e7b439dda61b6f8';
   static String recipes() => '';
   static String diets() => '';
   static String meals() => '';
